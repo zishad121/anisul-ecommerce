ed82a0d2ee079b3914cdcb38587e63a7bb00c40f
zishad121 <165453118+zishad121@users.noreply.github.com>
2024-04-19 14:30:52 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+
+let menuIcon =document.querySelector("#menu-icon")
+let menu =document.querySelector("#menu")
+
+
+
+
+
+
+
+
+menuIcon.addEventListener("click",()=>{
+
+
+if(menu.className==="hidden"){
+
+menu.classList.remove("hidden")
+
+}
+else{menu.classList.add("hidden")}
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
